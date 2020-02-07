ca4354c48ee81de09733ca6242c6149dcb4f0bb6
Valexr <valexr@gmail.com>
2020-02-07 13:46:20 +0300

    Add svelte & tw

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+
+	export let name;
+</script>
+
+<style>
+	h1 {
+		/*@apply bg-black text-white;*/
+	}
+	
+</style>
+
+<Tailwindcss />
+<h1 class="bg-black text-white">Hello {name}!</h1>
