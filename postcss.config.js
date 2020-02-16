aec870aa193cf0b0b6f85bc62b822562d6f31b3d
Valexr <valexr@gmail.com>
2020-02-16 23:41:16 +0300

    purgecss!!!

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,28 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
-  
-  whitelistPatterns: [/svelte-/, /fa-/],
-  // rejected: true,
+    content: ['./src/**/*.html', './src/**/*.svelte'],
+    whitelistPatterns: [/svelte-/, /fa-/],
 
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    // /svelte-\S+/ig
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+
+    // defaultExtractor: content => {
+    //     const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
+    //     const matchedTokens = [];
+    //     let match = regExp.exec(content);
+
+    //     while (match) {
+    //         if (match[0].startsWith('class:'))
+    //             matchedTokens.push(match[0].substring(6));
+    //         else matchedTokens.push(match[0]);
+    //         match = regExp.exec(content);
+    //     }
+
+    //     return matchedTokens;
+    // }
 });
 
-const production = !process.env.ROLLUP_WATCH
+const production = !process.env.ROLLUP_WATCH;
 
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    ...(production ? [purgecss] : [])
-  ]
+    plugins: [require('tailwindcss'), ...(production ? [purgecss] : [])]
 };
--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,12 @@
 <script>
 	import Tailwindcss from './Tailwindcss.svelte';
-	import { 
-		Header, 
-		// Nav, 
-		FlextypeSvg, 
-		Html5, 
-		Table
-	}  from './components';	
-	import Icon from 'svelte-awesome';
-	import Nav from "./components/Nav.svelte";
+	import {
+		Nav,
+		Header,
+		Table,
+		FlextypeSvg,
+		Html5
+	} from './components';
 </script>
 
 <Tailwindcss />
@@ -21,33 +19,35 @@
 
 		<main class="main">
 			<Table />
-
-			<Icon />
-
 			<FlextypeSvg size="180" />
 			<Html5 />
-		    <svg 
-		    	class="svg-ico"
-		    	width="24"
+			<svg
+				class="svg-ico"
+				width="24"
 				height="24"
 				fill="none"
 				stroke="currentColor"
 				stroke-width="2"
 				stroke-linecap="round"
-				stroke-linejoin="round">
-			    <use xlink:href="feather-sprite.svg#activity" />
-		  	</svg>
+				stroke-linejoin="round"
+			>
+				<use xlink:href="feather-sprite.svg#activity" />
+			</svg>
 		</main>
 	</div>
 </div>
 
 <style type="text/scss">
-	.base { @apply flex h-full w-full;
-		.content { @apply flex flex-col h-full w-full;
-			.main { @apply w-full h-full bg-gray-100 p-6;}
+	.base {
+		@apply flex h-full w-full;
+		.content {
+			@apply flex flex-col h-full w-full;
+			.main {
+				@apply w-full h-full bg-gray-100 p-6;
+			}
 		}
 	}
-	.svg-ico{
+	.svg-ico {
 		display: inline-block;
 		color: currentColor;
 		width: 24px;
